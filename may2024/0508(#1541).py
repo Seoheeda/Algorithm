d48108dcda37db3f00084a8906e2de782a6a4c6a
Seoheeda <1015aliciay@gmail.com>
2024-05-10 23:54:33 +0900

    #1541 잃어버린 괄호

--- may2024/0508(#1541).py
+++ may2024/0508(#1541).py
@@ -1,46 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-sick = input()
-operations = []
-plus = []
-minus = []
-ans = 0
+sick = input().split('-')
 
-for i in range(len(sick)):
-    if sick[i] == '+' or sick[i] == '-':
-        operations.append(i)
-    if sick[i] == '+':
-        plus.append(i)
-    elif sick[i] == '-':
-        minus.append(i)
+nums = []
 
-if  len(minus) == 0:
-    if len(plus) == 1:
-            ans += int(sick[: plus[0]])
-            ans += int(sick[plus[0] + 1 :])
-    else:
-        for i in range(len(plus)):
-            if i == 0:
-                ans += int(sick[: plus[i]])
-            elif i == len(plus) - 1:
-                ans += int(sick[plus[i - 1] : plus[i]])
-                ans += int(sick[plus[i] :])
-            else:
-                ans += int(sick[plus[i - 1] : plus[i]])
-elif len(plus) == 0:
-    if len(minus) == 1:
-            ans += int(sick[: minus[0]])
-            ans -= int(sick[minus[0] + 1 :])
-    else:
-        for i in range(len(minus)):
-            if i == 0:
-                ans += int(sick[: minus[i]])
-            elif i == len(minus) - 1:
-                ans -= int(sick[minus[i - 1] + 1 : minus[i]])
-                ans -= int(sick[minus[i] + 1 :])
-            else:
-                ans -= int(sick[minus[i - 1] + 1 : minus[i]])
+for i in sick:
+    sum = 0
+    plus = i.split('+')
+    for j in plus:
+        sum += int(j)
+    nums.append(sum)
 
+ans = nums[0]
+
+for i in range(1, len(nums)):
+    ans -= nums[i]
 
 print(ans)
