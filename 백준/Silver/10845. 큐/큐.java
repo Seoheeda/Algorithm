023f6659e6ab18216b780762908ee18a45dc575c
Seoheeda <127715698+Seoheeda@users.noreply.github.com>
2024-07-27 00:35:04 +0900

    [Silver IV] Title: 큐, Time: 256 ms, Memory: 20352 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -9,6 +11,7 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		Queue<Integer> queue = new LinkedList<>();
 		int n = Integer.parseInt(br.readLine());
 		int back = 0;
@@ -25,38 +28,41 @@ public class Main {
 				break;
 			case "pop" :
 				if (queue.size() == 0) {
-					System.out.println(-1);
+					bw.write("-1\n");
 				} else {
-				System.out.println(queue.poll());
+				bw.write(queue.poll() + "\n");
 				}
 				break;
 			case "size" :
-				System.out.println(queue.size());
+				bw.write(queue.size() + "\n");
 				break;
 			case "empty" :
 				if (queue.isEmpty()) {
-					System.out.println(1);
+					bw.write("1\n");
 				} else {
-					System.out.println(0);
+					bw.write("0\n");
 				}
 				break;
 			case "front" :
 				if (queue.size() == 0) {
-					System.out.println(-1);
+					bw.write("-1\n");
 				} else {
-				System.out.println(queue.peek());
+					bw.write(queue.peek() + "\n");
 				}
 				break;
 			case "back" :
 				if (queue.size() == 0) {
-					System.out.println(-1);
+					bw.write("-1\n");
 				} else {
-				System.out.println(back);
+					bw.write(back + "\n");
 				}
 				break;
 			}
 			
 		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
