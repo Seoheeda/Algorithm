c8efe9d8ba319940f735191c623b54380d079ba0
Seoheeda <127715698+Seoheeda@users.noreply.github.com>
2024-08-22 17:36:32 +0900

    [Gold II] Title: 빵집, Time: 344 ms, Memory: 34648 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -11,15 +11,6 @@ public class Main {
 	static int cnt;
 	static int[] dx = {-1, 0, 1};
 	
-	public static void print() {
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				System.out.print(arr[i][j]);
-			}
-			System.out.println();
-		}
-	}
-	
 	public static boolean dfs(int x, int y) {
 				
 		if (y == C - 1) {
@@ -36,7 +27,6 @@ public class Main {
 				 if (dfs(nx, ny)) {
 		                return true; 
 				 }
-				dfs(nx, ny);
 			}
 		}
 		
