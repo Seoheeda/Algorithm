ea03d293ca8a6fb13e0023b175e98e41e3650337
Seoheeda <127715698+Seoheeda@users.noreply.github.com>
2024-09-01 21:57:15 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,219 ms, Memory: 238,120 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,117 +2,104 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-// 노드 클래스
-class Node implements Comparable<Node> {
-	
-	int a;
-	int b;
-	int c;
+// 간선 클래스
+class Edge implements Comparable<Edge> {
+    int to;
+    int weight;
 
-	public Node(int a, int b, int c) {
-		super();
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
+    public Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
 
-	@Override
-	public int compareTo(Node o) {
-		if (this.c == o.c) {
-			if (this.b == o.b) {
-				return Integer.compare(this.a, o.a);
-			}
-			return Integer.compare(this.b, o.b);
-		}
-		return Integer.compare(this.c, o.c);
-	}	
+    @Override
+    public int compareTo(Edge o) {
+        return Integer.compare(this.weight, o.weight);
+    }
 }
 
 public class Solution {
-	
-	static int[] parents;
-	
-	// 부모 찾기
-	public static int find(int target) {
-		if (parents[target] == target) {
-			return target;
-		} else {
-			parents[target] = find(parents[target]);
-			return parents[target];
-		}
-	}
-	
-	// 부모 같으면 true, 다르면 false
-	public static boolean union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		
-		if (a == b) {
-			return false;
-		} else {
-			parents[b] = a;
-			return true;
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int t = 1; t <= T; t++) {
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			// 정점 개수
-			int V = Integer.parseInt(st.nextToken());
-			// 간선 개수
-			int E = Integer.parseInt(st.nextToken());
-			
-			// 1 ~ V 노드 정보 담기
-			parents = new int[V + 1];
-			for (int i = 1; i <= V; i++) {
-				parents[i] = i;
-			}
-			
-			// 간선 리스트
-			List<Node> list = new ArrayList<>();
-			for (int i = 0; i < E; i++) {
-				st = new StringTokenizer(br.readLine());
-				Node node = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-				list.add(node);
-			}
-			// 가중치 기준으로 오름차순 정렬
-			Collections.sort(list);
-			
-			int cnt = 0;
-			long sum = 0;
-			
-			for (Node node : list) {
-				if (union(node.a, node.b )) {
-					sum += node.c;
-					cnt++;
-					
-					if (cnt == E) {
-						break;
-					}
-				}
-			}
-			
-			sb.append("#").append(t).append(" ").append(sum).append("\n");
-		}
-		
-		bw.append(sb);
-		bw.flush();
-		bw.close();
-		
-	}
+
+    static List<Edge>[] graph;
+    static boolean[] visited;
+
+    public static long prim(int start, int V) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
+        long sum = 0;
+        int count = 0;
+
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
+            int to = current.to;
+            int weight = current.weight;
+
+            if (visited[to]) continue;
+
+            visited[to] = true;
+            sum += weight;
+            count++;
+
+            if (count == V) {
+                break;
+            };
+
+            for (Edge edge : graph[to]) {
+                if (!visited[edge.to]) {
+                    pq.add(edge);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            // 정점 개수
+            int V = Integer.parseInt(st.nextToken());
+            // 간선 개수
+            int E = Integer.parseInt(st.nextToken());
+
+            // 그래프 초기화
+            graph = new ArrayList[V + 1];
+            visited = new boolean[V + 1];
+
+            for (int i = 1; i <= V; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            // 간선 정보 입력
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                graph[a].add(new Edge(b, c));
+                graph[b].add(new Edge(a, c));
+            }
+
+            // 프림 알고리즘 실행 (임의의 시작 정점에서 시작)
+            long sum = prim(1, V);
+
+            sb.append("#").append(t).append(" ").append(sum).append("\n");
+        }
+
+        bw.append(sb);
+        bw.flush();
+        bw.close();
+    }
 }
