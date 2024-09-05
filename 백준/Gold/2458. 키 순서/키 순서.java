3687c55880819edef36cf2b0bb326524cc5d22e4
Seoheeda <127715698+Seoheeda@users.noreply.github.com>
2024-09-05 22:55:38 +0900

    [Gold IV] Title: 키 순서, Time: 1096 ms, Memory: 44508 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int[][] arr;
+	
+	static int bfsSmall(int target) {
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(target);
+
+		boolean[] visited = new boolean[N + 1];
+		visited[target] = true;
+		
+		int cntSmall = 0;
+		
+		while (!queue.isEmpty()) {
+			int stu = queue.poll();
+			
+			for (int i = 1; i < N + 1; i++) {
+				if (arr[i][stu] == 1 && visited[i] == false) {
+					queue.add(i);
+					visited[i] = true;
+					cntSmall++;
+				}
+			}
+		}
+		
+		return cntSmall;
+	}
+	
+	static int bfsBig(int target) {
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(target);
+
+		boolean[] visited = new boolean[N + 1];
+		visited[target] = true;
+		
+		int cntBig = 0;
+		
+		while (!queue.isEmpty()) {
+			int stu = queue.poll();
+			
+			for (int i = 1; i < N + 1; i++) {
+				if (arr[stu][i] == 1 && visited[i] == false) {
+					queue.add(i);
+					visited[i] = true;
+					cntBig++;
+				}
+			}
+		}
+		
+		return cntBig;
+	}
+	
+	
+    public static void main(String[] args) throws Exception {
+ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        	
+    	// 학생 수
+    	N = Integer.parseInt(st.nextToken());
+    	// 키 비교 횟수
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	arr = new int[N + 1][N + 1];
+    	
+    	for (int i = 0; i < M; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		int a = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		arr[a][b] = 1;        		
+    	}
+    	
+    	int cnt = 0;
+    	
+    	for (int i = 1; i < N + 1; i++) {
+    		if (bfsBig(i) + bfsSmall(i) == N - 1) {
+    			cnt++;
+    		}
+    	}
+    	
+    	System.out.println(cnt);
+    }
+}
