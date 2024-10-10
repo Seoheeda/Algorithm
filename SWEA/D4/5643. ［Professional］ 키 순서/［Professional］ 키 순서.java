a319620f0e13c0c14b0f36783ef9962aebb0f362
Seoheeda <127715698+Seoheeda@users.noreply.github.com>
2024-10-10 15:39:57 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,963 ms, Memory: 93,892 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,96 +1,69 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 	
 	static int N;
-	static int[][] arr;
-	
-	static int bfsSmall(int target) {
-		
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.add(target);
-
-		boolean[] visited = new boolean[N + 1];
-		visited[target] = true;
-		
-		int cntSmall = 0;
-		
-		while (!queue.isEmpty()) {
-			int stu = queue.poll();
-			
-			for (int i = 1; i < N + 1; i++) {
-				if (arr[i][stu] == 1 && visited[i] == false) {
-					queue.add(i);
-					visited[i] = true;
-					cntSmall++;
-				}
-			}
-		}
-		
-		return cntSmall;
-	}
-	
-	static int bfsBig(int target) {
-		
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.add(target);
-
-		boolean[] visited = new boolean[N + 1];
-		visited[target] = true;
-		
-		int cntBig = 0;
-		
-		while (!queue.isEmpty()) {
-			int stu = queue.poll();
-			
-			for (int i = 1; i < N + 1; i++) {
-				if (arr[stu][i] == 1 && visited[i] == false) {
-					queue.add(i);
-					visited[i] = true;
-					cntBig++;
-				}
-			}
-		}
-		
-		return cntBig;
-	}
-	
-	
+    static int[][] arr;      
+      
     public static void main(String[] args) throws Exception {
- 
+   
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+          
         int T = Integer.parseInt(br.readLine());
-        
+          
         for (int t = 1; t <= T; t++) {
-        	
-        	// 학생 수
-        	N = Integer.parseInt(br.readLine());
-        	// 키 비교 횟수
-        	int M = Integer.parseInt(br.readLine());
-        	
-        	arr = new int[N + 1][N + 1];
-        	
-        	for (int i = 0; i < M; i++) {
-        		StringTokenizer st = new StringTokenizer(br.readLine());
-        		int a = Integer.parseInt(st.nextToken());
-        		int b = Integer.parseInt(st.nextToken());
-        		arr[a][b] = 1;        		
-        	}
-        	
-        	int cnt = 0;
-        	
-        	for (int i = 1; i < N + 1; i++) {
-        		if (bfsBig(i) + bfsSmall(i) == N - 1) {
-        			cnt++;
-        		}
-        	}
-        	
-        	System.out.println("#" + t + " " + cnt);
+              
+            // 학생 수
+            N = Integer.parseInt(br.readLine());
+            // 키 비교 횟수
+            int M = Integer.parseInt(br.readLine());
+              
+            arr = new int[N + 1][N + 1];
+              
+            for (int i = 0; i < M; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                arr[a][b] = 1;              
+            }
+            
+            for (int i = 0; i < N + 1; i++) {
+            	for (int j = 0; j < N + 1; j++) {
+            		if (arr[i][j] != 1) {
+            			arr[i][j] = 1000;
+            		}
+            	}
+            }
+              
+            for (int k = 1; k <= N; k++) {
+            	for (int i = 1; i <= N; i++) {
+            		for (int j = 1; j <= N; j++) {
+            			if (arr[i][j] > arr[i][k] + arr[k][j]) {
+            				arr[i][j] = arr[i][k] + arr[k][j];
+            			}
+            		}
+            	}
+            }
+
+            int ans = 0;
+            for (int i = 1; i < N + 1; i++) {
+            	int top = 0;
+            	int down = 0;
+            	for (int a = 0; a < N + 1; a++) {
+            		if (arr[i][a] < 1000) {
+            			top++;
+            		}
+            		if (arr[a][i] < 1000) {
+            			down++;
+            		}
+            	}
+            	if (top + down == N - 1) {
+            		ans++;
+            	}
+            }
+            
+            System.out.println("#" + t + " " + ans);
         }
-    }
+	}
 }
